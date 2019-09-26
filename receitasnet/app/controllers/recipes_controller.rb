9a64a7b6e9e86e56517d9de2a9be718b96bbee98
wagner <wagner.dev1337@gmail.com>
2019-09-26 17:45:05 -0300

    usando ruby embedado na view

--- receitasnet/app/controllers/recipes_controller.rb
+++ receitasnet/app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
 
     def index
-        
+        @recipes = ['Strogonoff', 'Filé de Frango a Milanesa', 'Salmão Grelhado', 'Bacalhau Grelhado']
     end
 end
